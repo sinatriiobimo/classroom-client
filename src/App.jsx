ea60b4a029135075ae3755493acfe8b62eaf0c63
sinatriiobimo <sinatriobimbim@gmail.com>
2021-10-27 21:55:51 +0700

    feat(master): channel container

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie/es6'
-import './App.css'
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components'
 
+import 'stream-chat-react/dist/css/index.css'
+import './App.css'
+
+const cookies = new Cookies();
+
 const apiKey = '6n8kx7k8xzk9';
+const authToken = cookies.get("token");
+
 const client = StreamChat.getInstance(apiKey);
-const authToken = false;
+
+if(authToken) {
+    client.connectUser({
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        image: cookies.get('avatarURL'),
+        phoneNumber: cookies.get('phoneNumber'),
+        hashedPassword: cookies.get('hashedPassword'),
+    }, authToken);
+}
 
 const App = () => {
+    const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
 
     if(!authToken) return <Auth />
 
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
-                <ChannelListContainer/>
-                <ChannelContainer/>
+                <ChannelListContainer
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}/>
+                <ChannelContainer
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}/>
             </Chat>
         </div>
     )
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 import signInImage from '../assets/signup.jpg'
 
+const cookies = new Cookies()
+
 const initialState = {
     fullName: '',
     username: '',
@@ -23,10 +25,28 @@ const Auth = () => {
         // console.log(form)
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(form)
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'http://localhost:5000/auth';
+        const { data: {token, userId, hashedPassword} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL
+        });
+
+        cookies.set('token', token);
+        cookies.set('fullName', fullName);
+        cookies.set('username', username);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     const switchMode = () => {
